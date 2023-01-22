67c64ba636b25a53ca360c123e7c4626d2894a04
AntonioEduardo-Dev~ <antedu467@gmail.com>
2023-01-22 19:52:03 -0300

    aula 05 web

--- aulas-node-react/server/src/routes.ts
+++ aulas-node-react/server/src/routes.ts
@@ -76,7 +76,7 @@ export async function appRoutes(app: FastifyInstance) {
 
         const completedHabits = day?.dayHabits.map(dayHabit => {
             return dayHabit.habit_id
-        })
+        }) ?? []
 
         return {
             possibleHabits,
